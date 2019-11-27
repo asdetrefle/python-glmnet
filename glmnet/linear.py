dbb58272606638f6cd96b08e2714bc24f9ab535b
asdetrefle <qjchv@albatros>
2019-11-27 12:47:15 +0800

    remove last col containing more features than max

--- glmnet/linear.py
+++ glmnet/linear.py
@@ -367,6 +367,11 @@ class ElasticNet(BaseEstimator):
         nin = nin[:self.n_lambda_]
         self.coef_path_ = solns(_x.shape[1], ca, ia, nin)
 
+        n_features = np.count_nonzero(self.coef_path_, axis=0)
+        self.lambda_path_ = self.lambda_path_[n_features<=max_features]
+        self.intercept_path_ = self.intercept_path_[n_features<=max_features]
+        self.coef_path_ = self.coef_path_[n_features<=max_features]
+
         return self
 
     def decision_function(self, X, lamb=None):
